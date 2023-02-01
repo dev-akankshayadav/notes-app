3939e57202431cfcc40b8710d5c46133e32230d0
Akanksha Yadav <programmingwithyadavakanksha.40@gmail.com>
2023-02-01 16:04:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { FaTimes, FaPlus } from "react-icons/fa";
 
-function App() {
+export default function App() {
+  const [openModal, setOpenModal] = useState(false);
+  //adding state for Title and Description
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteDescription, setNoteDescription] = useState("");
+  const [notes, setNotes] = useState([{ title: "", description: "" }]);
+  console.log(notes);
+
+  function handleOpenModal() {
+    setOpenModal(true);
+    setNoteTitle("");
+    setNoteDescription("");
+  }
+
+  function handleCloseModal() {
+    setOpenModal(false);
+  }
+
+  // console.log(`Title: ${noteTitle}, Description: ${noteDescription}`);
+
+  function handleSave() {
+    const newNote = {
+      title: noteTitle,
+      description: noteDescription,
+    };
+    setNotes([...notes, newNote]);
+    handleCloseModal();
+  }
+
+  // console.log(notes);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="notes-app">
+        <h1> Notes App</h1>
+        <button
+          className="save-btn"
+          onClick={handleOpenModal}
+          title="add note"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <FaPlus />
+        </button>
+      </div>
+      {openModal ? (
+        <div className="modal">
+          <button
+            className="close-btn"
+            onClick={handleCloseModal}
+            title="close"
+          >
+            <FaTimes />
+          </button>
+          <input
+            className="title-here"
+            name="title"
+            type="text"
+            placeholder="Title-1"
+            onChange={(e) => setNoteTitle(e.target.value)}
+            value={noteTitle}
+          />
+          <input
+            type="text"
+            className="title-here"
+            placeholder="Write your notes here"
+            name="description"
+            onChange={(e) => setNoteDescription(e.target.value)}
+            value={noteDescription}
+          />
+          <button className="save-btn" title="save" onClick={handleSave}>
+            <FaPlus/>
+          </button>
+        </div>
+      ) : (
+        <>
+          {notes.map((note) => {
+            return (
+              <div className="note">
+                <p className="title">{note.title}</p>
+                <p className="description">{note.description}</p>
+              </div>
+            );
+          })}
+        </>
+      )}
+    </>
   );
 }
-
-export default App;
